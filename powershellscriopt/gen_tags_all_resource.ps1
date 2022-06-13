cd6ae4a0f5f45934445e6a17cec08d50908d9d4e
Rakesh Yadav <50994371+raakesh593812@users.noreply.github.com>
2022-06-13 22:33:18 +0530

    Create gen_tags_all_resource.ps1

---
+++ powershellscriopt/gen_tags_all_resource.ps1
@@ -0,0 +1,36 @@
+$date = Get-Date -UFormat("%m-%d-%y")
+$currentDir = $(Get-Location).Path
+$oFile = "$($currentDir)\List_Of_All_Azure_Resources_$($date).csv"
+ 
+if(Test-Path $oFile){
+    Remove-Item $oFile -Force
+}
+ 
+"SUBSCRIPTION_NAME, RESOURCE_GROUP_NAME,RESOURCE_NAME,TAGS" | Out-File $oFile -Append -Encoding ascii
+ 
+Get-AzSubscription | ForEach-Object{
+    $subscriptionId = $_.Id
+    $subscriptionName = $_.Name
+     
+    Set-AzContext -SubscriptionId $subscriptionId
+    Get-AzResourceGroup | ForEach-Object{
+        $resourceGroupName = $_.ResourceGroupName
+        Get-AzResource -ResourceGroupName $resourceGroupName | ForEach-Object{
+            $resourceName = $_.Name
+              
+            $resourceType = $_.ResourceType
+             
+            if(!([string]::IsNullOrEmpty($_.Tags))){
+                $tags = @()
+                $_.Tags.GetEnumerator() |ForEach-Object {
+                    [string]$tags += $_.key+ "=" + $_.value+ ";"
+                }
+            }
+            else{
+                $tags = ""
+            }
+             
+            "$subscriptionId,$resourceGroupName,$resourceName,$tags" | Out-File $oFile -Append -Encoding ascii
+        }
+    }
+}
